25c9e6f6bd245d72e3f59252bb5226e0bf770dc2
HarikesMenon <iamharikes@gmail.com>
2016-09-26 14:14:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.get('/counter', function (req,res) {
     
 });
 
+var names = [];
+app.get('/submit-name',function(req,res) {
+    //Get the name from request 
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 app.get('/ManchesterUnited', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'ManchesterUnited.html'));
 });
@@ -41,13 +49,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names = [];
-app.get('/submit-name',function(req,res) {
-    //Get the name from request 
-    var name = req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
